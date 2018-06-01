1f05c4189b0ba2b4a12ac9926b8f9e3caed61f11
Gunn <sgunn@lp-b050623-osx.hq.corp.viasat.com>
2018-06-01 12:02:35 -0500

    add new c and text file

---
+++ in_print_statement.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+
+    printf("4916561883525228");
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    // True if the number is perfectly divisible by 2
+    if(number % 2 == 0)
+        printf("%d is even.", number);
+    else
+        printf("%d is odd.", number);
+
+    return 0;
+}
